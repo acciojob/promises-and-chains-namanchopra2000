b893c9ee6fde5da6fb80fde848a4faa907a00fe6
namanchopra2000 <117727607+namanchopra2000@users.noreply.github.com>
2023-03-20 02:19:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+function EligibleForvVote() {
+	var age = document.getElementById("age").value ;
+var name = document.getElementById("name").value ;
+	const mypromise = new Promise((resolve , reject) => {
+		if(age >=18){
+			setTimeout(()=>{
+				resolve(true)
+				alert("Welcome, " + name + "You can vote.")
+			},4000)
+		}
+		else {
+			setTimeout(()=>{
+				reject(false)
+				alert("Oh sorry" name "You aren't old enough.")
+			},4000)
+		}
+	})
+	
+}
